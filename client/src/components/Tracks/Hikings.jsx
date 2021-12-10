e5e7c0e9ddc53db9ebf2b9a35c228599398488ae
MrSociety404 <cstfabrice@gmail.com>
2021-12-10 12:27:10 +0100

    🌲Fix hicking bug

--- client/src/components/Tracks/Hikings.jsx
+++ client/src/components/Tracks/Hikings.jsx
@@ -1,29 +1,58 @@
-
 import "./Hikings.scss";
 
 //Import cards
-import Card from './Card.jsx';
+import Card from "./Card.jsx";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Loading from '../Common/Loading'
+import Button from '../Common/Button'
+import { useNavigate } from "react-router-dom";
 
-const Hikings=()=>{
-            
-     
-    return(
-        <div className="cards">
-            {/* {Card.map(card=>{
-                return(
-                    <div>{Card.card}</div>
-                )
-            })} */}
-       <Card />
-       </div>
-    )
-}
+const Hikings = () => {
+  const [hikings, setHikings] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
 
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    fetchHikings();
+  }, []);
 
+  const fetchHikings = async () => {
+    const response = await axios.get("http://localhost:80/api/hiking", {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+    if (response.status === 200) {
+      setHikings(response.data.data);
+      setIsFetching(false);
+    }
+  };
 
+  const goCreate = () => {
+    navigate('/manage')
+  }
 
-
-
+  return (
+    <>
+      {isFetching ? (
+        <Loading />
+      ) : (
+        <main className="hikings">
+          <div className="hikings__box">
+            <h2 className="hikings__title">ALL THE HIKINGS</h2>
+            <Button to="/manage" className="hikings__btn" onClick={goCreate} content='ADD YOURS' />
+          </div>
+          <section className="hikings__list">
+            {hikings.map(hiking => (
+              <Card key={hiking.id} hiking={hiking} />
+            ))}
+          </section>
+        </main>
+      )}
+    </>
+  );
+};
 
 export default Hikings;
--- client/src/pages/Tracks.jsx
+++ client/src/pages/Tracks.jsx
@@ -4,11 +4,27 @@ import Hikings from"../components/Tracks/Hikings";
 // SVG
 import {ReactComponent as Background} from '../assets/svg/background.svg'
 import {ReactComponent as DoubleChevron} from '../assets/svg/double_chevron.svg'
+import { useRef } from "react";
 
 const Tracks = () => {
+
+  const titleEl = useRef()
+
+
+  window.addEventListener('scroll', () => {
+    if(window.scrollY > 220 && titleEl.current) {
+      titleEl.current.classList.add('hidden')
+    } else if(window.scrollY < 220 && titleEl.current) {
+      titleEl.current.classList.remove('hidden')
+    }
+  })
+
   return (
-    <main className="tracks">
-      <Background className="tracks__backgroundIllu" />
+    <main className="tracks" >
+      <div className="tracks__head">
+        <Background className="tracks__backgroundIllu" />
+        <h1 className="tracks__title" ref={titleEl} >HIKING TRACK</h1>
+      </div>
       <DoubleChevron className="tracks__doubleChevron" />
       <Hikings />
 
