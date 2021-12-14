10adf20e1317a3206606a360fee85cf1c641bfc9
MrSociety404 <cstfabrice@gmail.com>
2021-12-14 16:56:36 +0100

    add

--- server/src/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-echo "Index";
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-
