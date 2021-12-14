d8e07aa83126f286ee130f06b819792ae2e92d65
MrSociety404 <cstfabrice@gmail.com>
2021-12-14 17:25:46 +0100

    fix level card

--- src/components/Tracks/Card.jsx
+++ src/components/Tracks/Card.jsx
@@ -4,39 +4,21 @@ import { ReactComponent as Distance } from "../../assets/svg/distance.svg";
 import { useNavigate } from "react-router-dom";
 
 import "./Hikings.scss";
-import { useEffect, useState } from "react";
 
 const Card = ({ hiking }) => {
   const navigate = useNavigate();
 
-  const [level, setLevel] = useState(parseInt(hiking.level));
+  const level = ["Easy", "Medium", "Hard", "Hardcore"];
 
   const showDetails = () => {
     navigate(`/view/${hiking.id}`);
   };
 
-  useEffect(() => {
-    switch (level) {
-      case 1:
-        setLevel("Medium");
-        break;
-      case 2:
-        setLevel("Hard");
-        break;
-      case 3:
-        setLevel("Hardcore");
-        break;
-      default:
-        setLevel("Easy");
-        break;
-    }
-  }, []);
-
   return (
     <article onClick={showDetails} className="card">
       <img className="card_img_top" alt={hiking.name} src={hiking.image}></img>
       <div className="card__container">
-        <p className="card__level">{level}</p>
+        <p className="card__level">{level[hiking.level]}</p>
         <h5 className="card__name"> {hiking.name} </h5>
         <div className="card__icons">
           <Chronometer />
